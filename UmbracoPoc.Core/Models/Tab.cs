599d941888820fda3722e753b182c9a5e796d234
Robert Ghafoor <robert.ghafoor@gmail.com>
2020-10-21 15:39:08 +0200

    .

---
+++ UmbracoPoc.Core/Models/Tab.cs
@@ -0,0 +1,44 @@
+﻿using BlueLeet.UCodeFirst;
+
+namespace UmbracoPoc.Web.Business.Models
+{
+    [TabRoot]
+    public enum Tab
+    {
+        [EnumName("#ContentTab")]
+        Content,
+
+        [EnumName("#PushTab")]
+        Push,
+
+        [EnumName("#ModuleContentTab")]
+        ModuleContent,
+
+        [EnumName("#PageTab")]
+        Page,
+
+        [EnumName("#SeoTab")]
+        Seo,
+
+        [EnumName("#NavigationTab")]
+        Navigation,
+
+        [EnumName("#SiteTab")]
+        Site,
+
+        [EnumName("#MailSettingsTab")]
+        MailSettings,
+
+        [EnumName("#PackageTab")]
+        Package,
+
+        [EnumName("#FooterTab")]
+        Footer,
+
+        [EnumName("#GlobalsTab")]
+        Globals,
+
+        [EnumName("#AppSectionSettingsTab")]
+        AppSectionSettings
+    }
+}
